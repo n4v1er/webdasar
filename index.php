5b7344bb022369aa11e18a2a2a3e7b384807d52e
n4v1er <zenaalmira891@gmail.com>
2023-05-11 13:57:29 +1000

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nama_kamu = "Tuti";
+    $umur = 5;
+    // tanda petik dua
+    /*
+    echo "Hello World, Welcome ".$nama_kamu;
+    echo " Umur Kamu ".$umur;
+    echo " Tahun";
+    echo "<br/>";
+    */
+    echo "Hello World, welcome ".$nama_kamu." umur kamu ".$umur." tahun";
+    echo "<br/>";
+    echo "Berarti $nama_kamu lahir di tahun ";
+    echo 2023-$umur;
+
+    // tanda petik satu
+    // echo 'Hello World';
+
+?>
